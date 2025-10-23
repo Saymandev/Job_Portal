6f38bf60767a9db915fb505ca18195b6ec53d695
Saymandev <devrabbi255@gmail.com>
2025-10-23 13:48:11 +0600

    build issue solve

--- apps/backend/src/modules/interviews/interviews.module.ts
+++ apps/backend/src/modules/interviews/interviews.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Application, ApplicationSchema } from '../applications/schemas/application.schema';
+import { Job, JobSchema } from '../jobs/schemas/job.schema';
 import { MailModule } from '../mail/mail.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { InterviewPrepService } from './interview-prep.service';
@@ -14,6 +15,7 @@ import { Interview, InterviewSchema } from './schemas/interview.schema';
     MongooseModule.forFeature([
       { name: Interview.name, schema: InterviewSchema },
       { name: Application.name, schema: ApplicationSchema },
+      { name: Job.name, schema: JobSchema },
     ]),
     MailModule,
     NotificationsModule,
